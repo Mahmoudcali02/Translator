000e91b8b0a4eb7f50583e14abc9832025aa237f
Abdifatah <abdifatah.101@outlook.com>
2021-07-23 22:47:11 +0100

    Small updates

--- Translator.js
+++ Translator.js
@@ -6,47 +6,7 @@ var resultEL = $('#result')
 
 let currentLang = "ru"
 
-'use strict';
 
-document.addEventListener('DOMContentLoaded', function () {
-
-  // Dropdowns
-
-  var $dropdowns = getAll('.dropdown:not(.is-hoverable)');
-
-  if ($dropdowns.length > 0) {
-    $dropdowns.forEach(function ($el) {
-      $el.addEventListener('click', function (event) {
-        event.stopPropagation();
-        $el.classList.toggle('is-active');
-      });
-    });
-
-    document.addEventListener('click', function (event) {
-      closeDropdowns();
-    });
-  }
-
-  function closeDropdowns() {
-    $dropdowns.forEach(function ($el) {
-      $el.classList.remove('is-active');
-    });
-  }
-
-  // Close dropdowns if ESC pressed
-  document.addEventListener('keydown', function (event) {
-    var e = event || window.event;
-    if (e.keyCode === 27) {
-      closeDropdowns();
-    }
-  });
-
-  // Functions
-
-  function getAll(selector) {
-    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
-  }
-});
 
 $("#translate").on("click", translate)
 
@@ -72,5 +32,3 @@ function changeLang() {
 
 }
 
-/* user siongui was able to come up with a method to make using the bulma css framework more tolerable by giving us an event listener that removes the error caused by the dropdown, this is his github: https://github.com/siongui
-And this is the documentation he detailed his findings on https://siongui.github.io/2018/01/19/bulma-dropdown-with-javascript/# */
